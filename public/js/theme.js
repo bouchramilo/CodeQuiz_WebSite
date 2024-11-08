28cad5c3c8bd4af2e8b75b5f7ab9caef9dcc6be0
Miloudy bouchra <miloudybouchra01@gmail.com>
2024-11-08 17:28:42 +0100

    Dark mode du site 'CodeQuiz'

--- public/js/theme.js
+++ public/js/theme.js
@@ -1,36 +1,28 @@
+const imgDark = "images/background_n.png";
+const imgLight = "images/background_.jpg";
+const image = document.getElementById("bg");
+
 // Vérifiez et appliquez le thème enregistré dans le localStorage au chargement de la page
-document.addEventListener('DOMContentLoaded', () => {
-    const currentTheme = localStorage.getItem('theme') || 'light';
-    document.body.classList.add(currentTheme);
+document.addEventListener("DOMContentLoaded", () => {
+  const currentTheme = localStorage.getItem("theme") || "light";
+  document.body.classList.add(currentTheme);
+
+  const imgTheme = localStorage.getItem("imgtheme") || imgLight;
+  image.src = imgTheme;
 });
 
 // Fonction pour basculer entre les thèmes et sauvegarder le choix dans le localStorage
 function Theme() {
-    const body = document.body;
-    if (body.classList.contains('light')) {
-        body.classList.replace('light', 'dark');
-        localStorage.setItem('theme', 'dark');
-    } else {
-        body.classList.replace('dark', 'light');
-        localStorage.setItem('theme', 'light');
-    }
+  const body = document.body;
+  if (body.classList.contains("light")) {
+    body.classList.replace("light", "dark");
+    image.src = imgDark; // Nouvelle image
+    localStorage.setItem("theme", "dark");
+    localStorage.setItem("imgtheme", imgDark);
+  } else {
+    body.classList.replace("dark", "light");
+    image.src = imgLight; // Revenir à l'image initiale
+    localStorage.setItem("theme", "light");
+    localStorage.setItem("imgtheme", imgLight);
+  }
 }
-
-// const body =document.querySelector("body");
-// const button = document.querySelector("theme");
-
-// if( localStorage.getItem("theme")){
-//     body.className += localStorage.getItem("theme");
-// }
-
-// button.addEventListener("click" , () => {
-//     if(localStorage.getItem("theme") === "dark" ){
-//         localStorage.removeItem("theme");
-//         body.classList = "" ;
-
-//     }
-//     else{
-//         localStorage.setItem("theme" , "dark");
-//         body.className = "dark" ;
-//     }
-// })
